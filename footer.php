e85400145c4e8a1aefd49ca04361e035f2be913d
Bionic Leha <bionic.mods@gmail.com>
2017-07-09 07:26:00 +0300

    Init main.js and Blog description on footer
    Took 2 hours 58 minutes

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
 <?php ?>
 <footer class="footer">
     <div class="container">
-        <span>Place sticky footer content here.</span>
+        <span><?php bloginfo('description'); ?></span>
     </div>
 </footer>
 <script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/jquery.min.js"></script>
-<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/bootstrap.min.js"></script>
 <script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/tether.min.js"></script>
+<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/main.js"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
