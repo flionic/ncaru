f29cfc7e8900b4ceeb96dff1786ff0561cfff333
Bionic Leha <bionic.mods@gmail.com>
2017-08-07 05:48:44 +0300

    Dropdown menu on hover
    Took 31 hours 18 minutes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 /**
  * Created by Bionic on 09.07.2017.
  */
+
+// carousel hover dropdown
+jQuery(function($) {
+    $('.dropdown').on('mouseenter mouseleave click tap', function () {
+        $(this).toggleClass("show");
+    });
+});
