514898ed64417fb184d1837528aea02c51a13cc9
Bionic Leha <bionic.mods@gmail.com>
2017-08-07 05:01:19 +0300

    Future page
    Took 30 hours 41 minutes

---
+++ dev-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Future page
+ */
+/**
+ * Created by PhpStorm.
+ * User: Bionic
+ * Date: 07.08.2017
+ * Time: 4:47
+ */
+
+get_header(); ?>
+    <div class="container">
+        <h1 style="padding: 20px 0; text-align: center;">Данная страница находится в разработке</h1>
+    </div>
+<?php get_footer();
+?>
