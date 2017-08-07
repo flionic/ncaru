2dd103efd39a94252545cb02059ed1c579289f1e
Bionic Leha <bionic.mods@gmail.com>
2017-08-07 05:02:23 +0300

    Added date and place to events
    Took 30 hours 42 minutes

--- single.php
+++ single.php
@@ -10,19 +10,24 @@
     <div class="container">
         <div class="jumbotron">
             <?php if (have_posts()): while (have_posts()): the_post(); ?>
-                <div class="media">
-                    <div class="col-8">
+                <div class="media-">
+                    <div class="col-12 col-md-7" style="float: left">
                         <blockquote class="blockquote">
+                            <small class="text-muted"><?php the_category(' '); ?></small><br>
                             <h1 class="post-title" style="color: #000"><?php the_title(); ?></h1>
-                            <?php edit_post_link( __( 'Редактировать'), '', '<br>', 0, 'post-edit-link btn btn-primary btn-sm' ); ?>
-                            <small class="text-muted"><?php the_time('d.m.y H:i'); ?>, <?php the_category( ', ' ); ?></small>
+                            <?php edit_post_link(__('Редактировать'), '', '<br>', 0, 'post-edit-link btn btn-primary btn-sm'); ?>
+                            <small class="text-muted">Дата события: <?php echo get_post_meta(get_the_ID(), 'Дата', true); ?></small><br>
+                            <small class="text-muted">Место: <?php echo get_post_meta(get_the_ID(), 'Место', true); ?></small>
                         </blockquote>
-                        <?php if (get_option('buyticket_btn') == '1') : echo '<a href="#" class="btn btn-warning">Купить билеты</a>'; endif;?>
+                        <?php if (get_option('buyticket_btn') == '1') : echo '<a href="#" class="btn btn-warning">Купить билеты</a>'; endif; ?>
                         <hr class="my-4">
-                        <p class="post-text"><?php the_content(); ?></p>
                     </div>
-                    <div class="col-4">
+                    <div class="col-12 col-md-5" style="float: right">
                         <img class="img-fluid rounded" src="<?php the_img_url(); ?>" alt="<?php the_title(); ?>">
+                        <div class="p-3 clearfix"></div>
+                    </div>
+                    <div class="col-12">
+                        <p class="post-text"><?php the_content(); ?></p>
                     </div>
                 </div>
             <?php endwhile; endif; ?>
