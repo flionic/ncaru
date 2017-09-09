2fb7e329e9601279b3bb91221c2aeb039038dbe0
Bionic Leha <bionic.mods@gmail.com>
2017-09-09 12:52:18 +0300

    Added social buttons
    Took 7 hours 14 minutes

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <?php ?>
 <footer class="footer">
     <div class="container">
+        <div class="social">
+            <?php get_template_part('social'); ?>
+        </div>
         <span><?php echo get_option('footer_text'); ?></span>
     </div>
 </footer>
