0cb34b291f75836a967607cca41fd8cd6fbb188d
Bionic Leha <bionic.mods@gmail.com>
2017-09-09 12:52:51 +0300

    Social buttons template
    Took 7 hours 15 minutes

---
+++ social.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bionic
+ * Date: 09.09.2017
+ * Time: 10:57
+ */
+?>
+<a class="ico ico-vk" href="https://vk.com/nca" title="Мы Вконтакте" target="_blank"></a>
+<a class="ico ico-fb" href="" title="Мы в Фейсбуке" target="_blank"></a>
+<a class="ico ico-ig" href="https://www.instagram.com/ncalive/" title="Мы в Instagram" target="_blank"></a>
+<a class="ico ico-kn" href="http://www.cosmonavt.su/" title="Сайт-партнер 'Космонавт'" target="_blank"></a>
+<a class="ico ico-mb" href="http://muzbilet.ru/" title="Сайт-партнер 'Музбилет'" target="_blank"></a>
+<a class="ico ico-sk" href="http://www.symphokino.ru/" title="Сайт-партнер 'Симфоническое Кино'" target="_blank"></a>
